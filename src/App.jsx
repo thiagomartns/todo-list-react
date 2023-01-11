1e5e6f2a910ba786d97db148227210805ef8e8ce
thiago <tmg3.mn@gmail.com>
2023-01-11 10:37:53 -0300

    Componente 'tasks' adicionado

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import Header from "./components/header/Header";
 import Input from "./components/input/Input";
 import './App.scss';
+import Tasks from "./components/tasks/Tasks";
 
 
 function App() {
@@ -9,6 +10,7 @@ function App() {
       <Header />
       <div className="appContainer">
         <Input />
+        <Tasks />
       </div>
     </div>
   );
---
+++ src/components/tasks/Tasks.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import './tasks.scss'
+
+const Tasks = () => {
+  return (
+    <div className='tasks'>
+      <div className="tasksCounter">
+        <div className="tasksCreated">
+          <p>Tarefas Criadas</p>
+          <span className='counterSpan'>0</span>
+        </div>
+        <div className="tasksCompleted">
+          <p>Concluídas</p>
+          <span className='counterSpan'>0</span>
+        </div>
+      </div>
+      <div className="tasksAppContainer">
+        <img src="/assets/img/ClipBoard.svg" alt="" />
+        <p className='noTask-1'>Você ainda não tem tarefas cadastradas</p>
+        <p className='noTask-2'>Crie tarefas e organize seus itens a fazer</p>
+      </div>
+    </div>
+  )
+}
+
+export default Tasks
